c38af8c7543e4f38246eedd1f380e690be09f5ea
Spark4545 <lerouxpaulhenri@gmail.com>
2024-01-02 14:44:36 +0000

    [task 0] bug fixing$

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -17,14 +17,30 @@ def canUnlockAll(boxes):
     explored = []
     frontier = [0]
     while True:
+        # If there are no boxes to explore
         if frontier == []:
             if len(explored) == len(boxes):
                 return True
             else:
                 return False
+        if frontier[0] > len(boxes):
+            return False
         for key in boxes[frontier[0]]:
+            # Add the current box to the explored list
             if frontier[0] not in explored:
                 explored.append(frontier[0])
+            # Case when the box has no key in it
+
+            # if the key is for an unexplored box, add the key to the frontier
             if key not in explored:
                 frontier.append(key)
+        if boxes[frontier[0]] == []:
+            explored.append(frontier[0])
+        # Remove the explored box from the frontier
         frontier.pop(0)
+
+        # print("Explored :", end="")
+        # print(explored)
+        # print("Frontier :", end="")
+        # print(frontier)
+        # print("=========")
