be3a19b55bc8c521dfee53861eb7b661028f7c79
Spark4545 <lerouxpaulhenri@gmail.com>
2024-01-05 22:15:39 +0100

    test

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -13,20 +13,25 @@ def canUnlockAll(boxes):
         possibility (boolean): if all the boxes can be opened
     """
 
+    # Using a search algorithm
     opened = []
     keys = [0]
     while True:
-        if keys == []:
-            if len(boxes) == len(opened):
+        # Exit condition
+        if keys == []:  # When all keys have been used
+            if len(boxes) == len(opened):  # if the number of boxes is equal to the number of opened boxes
                 return True
             else:
                 return False
 
+        # Get the key and remove it from the list
         current_key = keys[0]
         keys.pop(0)
 
+        # If the current key opens a box that exists
         if current_key < len(boxes):
             opened.append(current_key)
+            # Main loop
             for key in boxes[current_key]:
                 if key not in opened and key not in keys:
                     keys.append(key)
