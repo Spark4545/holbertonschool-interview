3a7bce3277e741713f54ab021f08bc406fc8701f
Paul-Henri Leroux <124456232+spaarky@users.noreply.github.com>
2024-09-04 12:55:55 +0200

    Update substring.h

--- substring/substring.h
+++ substring/substring.h
@@ -9,7 +9,7 @@ int *find_substring(char const *s, char const **words, int nb_words, int *n);
 int *init_indices(int len);
 int *init_count(int nb_words);
 int check_words(const char *s, const char **words, int i,
-                int nb_words, int word_len, int *count);
+				int nb_words, int word_len, int *count);
 int add_index_if_match(int *indices, int *n, int i, int match);
 
 #endif /* _SUBSTRING_H */
