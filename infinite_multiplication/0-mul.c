ad20648bc9c043168404c1df1b4b652e85a6ac2c
Paul-Henri Leroux <124456232+spaarky@users.noreply.github.com>
2024-09-05 12:57:08 +0200

    Update 0-mul.c

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -1,119 +1,97 @@
-#include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdio.h>
+#include "holberton.h"
+
 
 /**
- * display_error_and_exit - Display "Error" and exit with status 98
- *
- * Return: void
- */
-void display_error_and_exit(void)
+ * print_error - prints "Error" and exits with status 98
+*/
+void print_error(void)
 {
-	printf("Error\n");
-	exit(98);
+char *error = "Error\n";
+while (*error)
+{
+_putchar(*error);
+error++;
+}
+exit(98);
 }
 
 /**
- * is_number - Check if a character is a number
- * @c: The character to check
- *
- * Return: 1 if c is a number, 0 otherwise
- */
-int is_number(char c)
+ * _isdigit - check if a string contains only digits
+ * @str: string to check
+*/
+void _isdigit(char *str)
 {
-	return (c >= '0' && c <= '9');
+int i;
+
+for (i = 0; str[i]; i++)
+{
+if (!(str[i] >= '0' && str[i] <= '9'))
+print_error();
+}
+}
+
+
+/**
+ * _strlen - return length of string
+ * @str: string to check
+ * Return: length of string
+*/
+int _strlen(char *str)
+{
+int len = 0;
+while (str[len])
+len++;
+return (len);
 }
 
 /**
- * check_arguments - Validate the arguments
- * @argc: The number of arguments
- * @argv: The arguments
- *
- * Return: void
- */
-void check_arguments(int argc, char *argv[])
-{
-	int i, j;
-
-	if (argc != 3)
-	{
-		display_error_and_exit();
-	}
-	for (i = 1; i < 3; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!is_number(argv[i][j]))
-			{
-				display_error_and_exit();
-			}
-		}
-	}
-}
-
-/**
- * calculate_product - Multiply two numbers
- * @num1: The first number
- * @num2: The second number
- *
- * Return: void
- */
-void calculate_product(char *num1, char *num2)
-{
-	int len1, len2, i, j, prod, sum;
-	int *result;
-
-	len1 = strlen(num1);
-	len2 = strlen(num2);
-	result = (int *)calloc(len1 + len2, sizeof(int));
-	if (result == NULL)
-	{
-		display_error_and_exit();
-	}
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			prod = (num1[i] - '0') * (num2[j] - '0');
-			sum = prod + result[i + j + 1];
-			result[i + j + 1] = sum % 10;
-			result[i + j] += sum / 10;
-		}
-	}
-
-	i = 0;
-	while (i < len1 + len2 && result[i] == 0)
-	{
-		i++;
-	}
-	if (i == len1 + len2)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		for (; i < len1 + len2; i++)
-		{
-			printf("%d", result[i]);
-		}
-		printf("\n");
-	}
-	free(result);
-}
-
-/**
- * main - Entry point
- * @argc: The number of arguments
- * @argv: The arguments
- *
- * Return: 0 on success, 98 on failure
- */
+ * main - multiplies two positive numbers
+ * @argc: number of argument
+ * @argv: arguments
+ * Return: 0 on success, or 98 on failure
+*/
 int main(int argc, char *argv[])
 {
-	check_arguments(argc, argv);
-	calculate_product(argv[1], argv[2]);
-	return (0);
+int len1, len2;
+int *result;
+int i, j, sum, mul;
+
+if (argc != 3)
+print_error();
+_isdigit(argv[1]);
+_isdigit(argv[2]);
+len1 = _strlen(argv[1]);
+len2 = _strlen(argv[2]);
+result = malloc((len1 + len2) *sizeof(int));
+if (!result)
+print_error();
+
+for (i = 0; i < len1 + len2; i++)
+result[i] = 0;
+for (i = len1 - 1; i >= 0; i--)
+{
+for (j = len2 - 1; j >= 0; j--)
+{
+mul = (argv[1][i] - '0') * (argv[2][j] - '0');
+sum = mul + result[i + j + 1];
+result[i + j + 1] = sum % 10;
+result[i + j] += sum / 10;
+}
+}
+
+i = 0;
+while (i < len1 + len2 && result[i] == 0)
+i++;
+if (i == len1 + len2)
+_putchar('0');
+else
+while (i < len1 + len2)
+{
+_putchar(result[i] + '0');
+i++;
+}
+_putchar('\n');
+return (0);
 }
